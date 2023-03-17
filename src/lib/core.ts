f613799e9ce7b830d254cf22c4ce312f074e0436
sika7 <91676209+sika7@users.noreply.github.com>
2023-03-17 16:38:25 +0900

    coreにモジュールと設定クラスを追加

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,5 +1,37 @@
-export class Core {
+interface Config {
+  template: string;
+}
+
+class UniversalAdConfig {
+  config: Config[] = [];
+
   constructor() {}
 
-  main() {}
+  insertConfig(config: Config[]) {
+    this.config = config;
+  }
+
+  setConfig() {}
+}
+
+class UniversalAdModule {
+  constructor() {}
+
+  importModule() {}
+
+  appendModule() {}
+}
+
+export class Core {
+  config: UniversalAdConfig;
+  module: UniversalAdModule;
+
+  constructor() {
+    this.config = new UniversalAdConfig();
+    this.module = new UniversalAdModule();
+  }
+
+  main() {
+
+  }
 }
