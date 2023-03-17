c5037f7b2c0f95305aedd7114f2bb585c3b8c14a
sika7 <91676209+sika7@users.noreply.github.com>
2023-03-17 16:41:21 +0900

    一旦保存

--- src/main.ts
+++ src/main.ts
@@ -8,29 +8,28 @@ const core = new Core();
 core.main();
 
 
-const elm = document.querySelector<HTMLButtonElement>('#app')!;
+// const elm = document.querySelector<HTMLButtonElement>('#app')!;
 
-console.log("elm", elm);
-class HelloWorld extends HTMLElement {
-  connectedCallback() {
-    // this.textContent = "";
-
-    const shadow = this.attachShadow({ mode: "closed" });
-    shadow.innerHTML = `
-    <style>
-      p {
-        text-align: center;
-        font-weight: normal;
-        padding: 1em;
-        margin: 0 0 2em 0;
-        background-color: #eee;
-        border: 1px solid #666;
-      }
-    </style>
-          <p>テスト</p>
-
-    `;
-  }
-}
-
-customElements.define( 'hello-world', HelloWorld );
+// class HelloWorld extends HTMLElement {
+//   connectedCallback() {
+//     // this.textContent = "";
+// 
+//     const shadow = this.attachShadow({ mode: "closed" });
+//     shadow.innerHTML = `
+//     <style>
+//       p {
+//         text-align: center;
+//         font-weight: normal;
+//         padding: 1em;
+//         margin: 0 0 2em 0;
+//         background-color: #eee;
+//         border: 1px solid #666;
+//       }
+//     </style>
+//           <p>テスト</p>
+// 
+//     `;
+//   }
+// }
+// 
+// customElements.define( 'hello-world', HelloWorld );
