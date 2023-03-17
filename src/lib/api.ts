4b9722aa1d84ec0b82720be3aace9b59071eb8a1
sika7 <91676209+sika7@users.noreply.github.com>
2023-03-17 21:58:09 +0900

    api classを作成

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,32 @@
-import axios from 'axios';
+import axios from "axios";
 
 export class Api {
-
   constructor() {}
+
+  getRequest(path: string) {
+    return axios
+      .get(path)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
+
+  postRequest(path: string, param: object) {
+    return axios
+      .post(path, param)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 }
