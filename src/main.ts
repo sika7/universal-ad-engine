04e99b4ef2c0196d3ced513ceec39721956af8dc
sika7 <91676209+sika7@users.noreply.github.com>
2023-03-18 13:29:13 +0900

    不要な記述を削除

--- src/main.ts
+++ src/main.ts
@@ -3,29 +3,3 @@ import { Core } from "./lib/core";
 const core = new Core();
 core.main([{ template: 'default' }]);
 
-
-// const elm = document.querySelector<HTMLButtonElement>('#app')!;
-
-// class HelloWorld extends HTMLElement {
-//   connectedCallback() {
-//     // this.textContent = "";
-// 
-//     const shadow = this.attachShadow({ mode: "closed" });
-//     shadow.innerHTML = `
-//     <style>
-//       p {
-//         text-align: center;
-//         font-weight: normal;
-//         padding: 1em;
-//         margin: 0 0 2em 0;
-//         background-color: #eee;
-//         border: 1px solid #666;
-//       }
-//     </style>
-//           <p>テスト</p>
-// 
-//     `;
-//   }
-// }
-// 
-// customElements.define( 'hello-world', HelloWorld );
