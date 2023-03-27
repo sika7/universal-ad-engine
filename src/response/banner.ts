38ca7e3aee6d9674e7128cddec6ed55d3145d345
sika7 <91676209+sika7@users.noreply.github.com>
2023-03-28 00:03:38 +0900

    バナーを追加

---
+++ src/response/banner.ts
@@ -0,0 +1,7 @@
+export type TypeResponseBanner = {
+  url: string;
+  img_src: string;
+  alt?: string;
+  width: number;
+  height: number;
+}
--- src/template/default.ts
+++ src/template/default.ts
@@ -1,20 +1,36 @@
 import { IPluginTemplate } from "../lib/template-manager";
 import { IUniversalAdTemplate } from "../lib/universal-ad-template";
+import { TypeResponseBanner } from "../response/banner";
 
 class UniversalAdTemplate implements IUniversalAdTemplate {
   constructor() {}
 
+  url = "https://github.com/sika7/universal-ad-engine";
+  imgSrc =
+    "https://placehold.jp/3d4070/ffffff/300x300.png?text=%E5%BA%83%E5%91%8A";
+  height = 300;
+  width = 300;
+  alt = "";
+
   style(): string {
     return `
-    p {}
     `;
+  }
+
+  update(response: TypeResponseBanner) {
+    // ここでレスポンスデータをメンバ変数に与える
+    this.url = response.url;
+    this.imgSrc = response.img_src;
+    this.height = response.height;
+    this.width = response.width;
+    if (response.alt) this.alt = response.alt;
   }
 
   render(): string {
     return `
 <div>
-  <a href="https://github.com/sika7/universal-ad-engine" target="_blank">
-    <img src="https://placehold.jp/3d4070/ffffff/300x300.png?text=%E5%BA%83%E5%91%8A" alt="テスト" />
+  <a href="{{ url }}" target="_blank">
+    <img src="{{ imgSrc }}" alt="{{ alt }}" width="{{ width }}" height="{{ height }}" />
   </a>
 </div>
     `;
