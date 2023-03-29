13e7805585c54574a537bbb0f6407678a2e26655
sika7 <91676209+sika7@users.noreply.github.com>
2023-03-29 23:57:18 +0900

    型を追加

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,5 +1,6 @@
 import { TPluginTemplate } from "./lib/template-manager";
 import { IUniversalAdSetting, IUniversalAdApi } from "./lib/core";
 import { RequestType } from "./lib/api";
+import { TypeResponseBanner } from "./response/banner";
 
-export { TPluginTemplate, IUniversalAdSetting, IUniversalAdApi, RequestType };
+export { TPluginTemplate, IUniversalAdSetting, IUniversalAdApi, RequestType, TypeResponseBanner };
