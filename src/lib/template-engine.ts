c589773f2b350b1cc1db77ac1bc325b951a53bed
sika7 <91676209+sika7@users.noreply.github.com>
2023-03-30 23:00:00 +0900

    テンプレートエンジンを修正

--- src/lib/template-engine.ts
+++ src/lib/template-engine.ts
@@ -6,13 +6,13 @@ export function getMethodList(object: object) {
 
 function getObjectFromInstance(instance: any): Record<string, unknown> {
   const keys = Object.getOwnPropertyNames(instance);
-  const obj: any = {};
-  keys.forEach(key => {
-    if (typeof instance[key] !== 'function') {
-      obj[key] = instance[key];
-    }
-    if (typeof instance[key] === 'function') {
-      obj[key] = instance[key]();
+  const obj: Record<string, unknown> = {};
+  keys.forEach((key) => {
+    const data = instance[key];
+    if (typeof data === "function") {
+      obj[key] = data();
+    } else {
+      obj[key] = data;
     }
   });
   return obj;
@@ -38,8 +38,8 @@ function variableExpansion(
   variables: string[],
   object: Record<string, unknown>
 ): string {
-  variables.forEach(variable => {
-    const regex = new RegExp(`{{${variable}}}`, 'g');
+  variables.forEach((variable) => {
+    const regex = new RegExp(`{{${variable}}}`, "g");
     const result = getProperty(object, variable, "");
     html = html.replace(regex, result);
   });
@@ -52,4 +52,3 @@ export function render(text: string, object: object): string {
   const _object = getObjectFromInstance(object);
   return variableExpansion(_text, values, _object);
 }
-
