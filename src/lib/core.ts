d6d1db5d68bd39a37c2aa77d779c284e1612c51b
sika7 <91676209+sika7@users.noreply.github.com>
2023-03-30 22:10:55 +0900

    一つだけwebComponentが登録されるように変更

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -5,7 +5,12 @@ function attach(setting: IUniversalAdSetting | undefined) {
   if (!setting) return;
   const data = templateManager.find(setting.template);
   if (!data) throw new Error("No template registration found.");
-  return new WebComponentWrapper(setting.id, data.template(), data.api, setting.parameter);
+  return new WebComponentWrapper(
+    setting.id,
+    data.template(),
+    data.api,
+    setting.parameter
+  );
 }
 
 export interface IUniversalAdSetting {
@@ -16,7 +21,10 @@ export interface IUniversalAdSetting {
 
 class Core {
   constructor() {
-    customElements.define("universal-ad-unit", WebComponentWrapper);
+    const name = "universal-ad-unit";
+    if (!customElements.get(name)) {
+      customElements.define(name, WebComponentWrapper);
+    }
   }
 
   use(template: IPluginTemplate) {
