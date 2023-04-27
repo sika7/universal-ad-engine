edbae7abbc35478601c0bde2d358a94d051e8ed7
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-27 11:18:35 +0900

    validationを追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { isHttps } from "@sika7/validator/lib/plugins/isHttps";
+import { isInteger } from "@sika7/validator/lib/plugins/isInteger";
 import { isNumber } from "@sika7/validator/lib/plugins/isNumber";
+import { isString } from "@sika7/validator/lib/plugins/isString";
+import { isUrl } from "@sika7/validator/lib/plugins/isUrl";
 import { UniversalAd } from "./lib/core";
 import pluginUniversalAdTemplate from "./template/default";
 
 UniversalAd.validation(isNumber());
+UniversalAd.validation(isString());
+UniversalAd.validation(isHttps());
+UniversalAd.validation(isUrl());
+UniversalAd.validation(isInteger());
 
 UniversalAd.use(pluginUniversalAdTemplate("https://localhost:8000", "get"));
 UniversalAd.freezed();
