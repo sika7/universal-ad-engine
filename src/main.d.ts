36fbdc282bccea7900487a26cad0db29bd459ec1
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-28 14:36:38 +0900

    不要なファイルを削除

--- src/main.d.ts
+++
@@ -1,6 +0,0 @@
-import { TPluginTemplate } from "./lib/template-manager";
-import { IUniversalAdSetting, IUniversalAdApi } from "./lib/core";
-import { RequestType } from "./lib/api";
-import { TypeResponseBanner } from "./response/banner";
-
-export { TPluginTemplate, IUniversalAdSetting, IUniversalAdApi, RequestType, TypeResponseBanner };
