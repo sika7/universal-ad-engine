57c0b26c5e346963187d068f8a2eeae4488aad24
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-28 15:51:46 +0900

    ファイルを移動

--- example/main.ts
+++ example/main.ts
@@ -1,4 +1,4 @@
-import { UniversalAd } from "../src/lib/core";
+import { UniversalAd } from "../src/lib/manager/core";
 import pluginUniversalAdTemplate from "../src/template/default";
 
 UniversalAd.use(pluginUniversalAdTemplate("https://localhost:8000", "get"));
