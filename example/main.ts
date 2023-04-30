4053297df9684814876df1bf8931bf0cb4db9c32
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-30 19:38:06 +0900

    パスを修正

--- example/main.ts
+++ example/main.ts
@@ -1,5 +1,5 @@
-import { UniversalAd } from "../src/lib/manager/core";
-import pluginUniversalAdTemplate from "../src/template/default";
+import { UniversalAd } from "../lib/manager/core";
+import pluginUniversalAdTemplate from "../template/default";
 
 
 const template =  UniversalAd.make(pluginUniversalAdTemplate("https://localhost:8000", "get"));
