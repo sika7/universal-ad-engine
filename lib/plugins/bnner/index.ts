0d25c61ca8958467efa9f3f58c4bc5d0281dbed1
sika7 <91676209+sika7@users.noreply.github.com>
2023-05-03 16:40:18 +0900

    インデントを修正

--- lib/plugins/bnner/index.ts
+++ lib/plugins/bnner/index.ts
@@ -17,19 +17,19 @@ class BnnerTemplate implements Template {
 
   style(): string {
     return `
-div {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 300px;
-  height: 300px;
-  margin: auto;
-}
-.close {
-  display: none;
-}
+      div {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 300px;
+        height: 300px;
+        margin: auto;
+      }
+      .close {
+        display: none;
+      }
     `;
   }
 
